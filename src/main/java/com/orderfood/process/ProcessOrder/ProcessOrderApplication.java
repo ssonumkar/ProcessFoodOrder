47553f4b3343b5ca333f91f5a6aeebc2381da1ab
Sagar Sonumkar <sagarsonumakr31@gmail.com>
2024-07-22 13:36:58 +0200

    First commit

---
+++ src/main/java/com/orderfood/process/ProcessOrder/ProcessOrderApplication.java
@@ -0,0 +1,13 @@
+package com.orderfood.process.ProcessOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderfood/process/ProcessOrder/ProcessOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderfood.process.ProcessOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
